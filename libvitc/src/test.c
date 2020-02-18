7e707b517ea91ee55b8a9ef2ef7901e641ac482b
Kevin Klein <kevin@0x002A.com>
2020-02-18 16:40:26 +0100

    Setup project structure

---
+++ libvitc/src/test.c
@@ -0,0 +1,10 @@
+#include "test.h"
+
+#include <stdio.h>
+
+void
+testPrint()
+{
+  printf("Hello, %s", "world");
+}
+
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "banner.h"
+
+#include <test.h>
+
+int main(int argc, char* argv[]) {  	
+  printStartupBanner();
+  testPrint();
+  return 0;
+}
